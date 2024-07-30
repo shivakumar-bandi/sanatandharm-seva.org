30e135a49aa106552814487cf09b4c08ea0eebdc
shiva kumar <nanishiva2022001@gmail.com>
2024-07-30 14:47:43 +0530

    changed path in react api-url

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,5 +1,5 @@
 
 
-  //export const API_URL = "http://localhost:5000"
+  export const API_URL = "http://localhost:5000"
 
-export const API_URL ="https://backend-project-jmxk.onrender.com"
+//export const API_URL ="https://backend-project-jmxk.onrender.com"
