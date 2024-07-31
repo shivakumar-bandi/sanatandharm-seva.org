29aa3db2dfb4f756bd68427b09f6956a6ebe8fc8
shiva kumar <nanishiva2022001@gmail.com>
2024-07-31 09:43:08 +0530

    Your commit message

--- src/vendorDashboard/components/EventList.jsx
+++ src/vendorDashboard/components/EventList.jsx
@@ -3,16 +3,15 @@ import axios from 'axios';
 import EventCard from './EventCard';
 import { API_URL } from '../data/apiPath';
 import './EventList.css'; // Ensure this CSS file is imported
-import { useNavigate } from 'react-router-dom'; 
-
+import { useNavigate } from 'react-router-dom';
 import { useUser } from '../contexts/UserContext';
 
-const EventList = ({ eventheader,onEdit }) => {
+const EventList = ({ eventheader, onEdit }) => {
   console.log('Rendering EventList');
   const [events, setEvents] = useState([]);
-
   const { user } = useUser();
   const navigate = useNavigate();
+
   useEffect(() => {
     const fetchEvents = async () => {
       try {
@@ -22,43 +21,41 @@ const EventList = ({ eventheader,onEdit }) => {
         console.error('Error fetching events:', error);
       }
     };
-
     fetchEvents();
   }, []);
 
   const handleDelete = async (id) => {
-    if (window.confirm('Are you sure you want to delete this Event?')){
+    if (window.confirm('Are you sure you want to delete this Event?')) {
       try {
         await axios.delete(`${API_URL}/api/events/events/${id}`);
         setEvents(events.filter(event => event._id !== id));
       } catch (error) {
         console.error('Error deleting event:', error);
       }
-    };
     }
+  };
 
-    const canEditOrDelete = user && user.email === 'nanishiva2022001@gmail.com' && user.role === 'admin';
+  const canEditOrDelete = user && user.email === 'nanishiva2022001@gmail.com' && user.role === 'admin';
 
   return (
-  <>
-    <div className="header">
+    <>
+      <div className="header">
         {eventheader}
         {canEditOrDelete && (
           <button onClick={() => navigate('/add-event')} className="btn btn-primary">Add New Event</button>
         )}
       </div>
-    <div className="event-list">
-      
-      {events.map(event => (
-        <EventCard 
-          key={event._id} 
-          event={event} 
-          onEdit={onEdit} 
-          onDelete={handleDelete} 
-        />
-      ))}
-    </div>
-  </>
+      <div className="event-list">
+        {events.map(event => (
+          <EventCard
+            key={event._id}
+            event={event}
+            onEdit={onEdit}
+            onDelete={handleDelete}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
