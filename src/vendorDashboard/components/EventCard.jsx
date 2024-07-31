d9381d77b51291f4e23c45979d86ee6cd5b1490b
shiva kumar <nanishiva2022001@gmail.com>
2024-07-31 10:03:14 +0530

    Your commit message

--- src/vendorDashboard/components/EventCard.jsx
+++ src/vendorDashboard/components/EventCard.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import './EventCard.css'; // Ensure this file exists and is properly styled
 import { useUser } from '../contexts/UserContext';
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom';
+import { API_URL } from '../data/apiPath';
 
 const EventCard = ({ event, onEdit, onDelete }) => {
   const { user } = useUser();
-
   const navigate = useNavigate();
 
   const canEditOrDelete = user && user.email === 'nanishiva2022001@gmail.com' && user.role === 'admin';
 
   return (
     <div className="event-card">
-      {event.image && <img src={`http://localhost:5000/uploads/${event.image}`} alt={event.title} />}
+      {event.image && <img src={`${API_URL}/uploads/${event.image}`} alt={event.title} />}
       <h3>{event.title}</h3>
       <p>{event.description}</p>
       <p>Date: {new Date(event.date).toLocaleDateString()}</p>
       <p>Location: {event.location}</p>
       {canEditOrDelete && (
         <>
-   <button onClick={() => onEdit(event)}>Edit</button>
-   <button onClick={() => onDelete(event._id)}>Delete</button>
+          <button onClick={() => onEdit(event)}>Edit</button>
+          <button onClick={() => onDelete(event._id)}>Delete</button>
         </>
       )}
     </div>
