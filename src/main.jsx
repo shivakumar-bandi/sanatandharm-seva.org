553d370c13ad1f8bd2c85ab8e464dbb7c1255831
shiva kumar <nanishiva2022001@gmail.com>
2024-08-03 10:27:34 +0530

    Your commit change in side bar

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './vendorDashboard/contexts/UserContext.jsx'; // Import the UserProvider
+import { ThemeProvider } from './vendorDashboard/contexts/ThemeContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
