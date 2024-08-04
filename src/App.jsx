66a5d1f0093f4e08e3757ca1b3e1b871f8bf4d9c
shiva kumar <nanishiva2022001@gmail.com>
2024-08-04 20:59:02 +0530

    Your commit changes in app.js and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import LandingPage from './vendorDashboard/pages/LandingPage';
-import Login from './vendorDashboard/components/forms/Login';
-import Register from './vendorDashboard/components/forms/Register';
-import AddArticle from './vendorDashboard/components/forms/AddArticle';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import ArticleTable from './vendorDashboard/components/ArticleTable';
-import AddEvent from './vendorDashboard/components/forms/AddEvent';
-import EventList from './vendorDashboard/components/EventList';
-import AddFestival from './vendorDashboard/components/forms/AddFestival';
-import FestivalList from './vendorDashboard/components/FestivalList';
-import NotFound from './vendorDashboard/components/NotFound';
-import { API_URL } from './vendorDashboard/data/apiPath';
-import EditFestival from './vendorDashboard/components/EditFestival';
-import Welcome from './vendorDashboard/components/Welcome';
-import ArticleHead from './vendorDashboard/components/ArticleHead';
-import EventHeader from './vendorDashboard/components/EventHeader';
-import FestivalHeader from './vendorDashboard/components/FestivalHeader';
-import Donation from './vendorDashboard/components/Donation';
-import Team from './vendorDashboard/components/Team';
-import Updates from './vendorDashboard/components/Updates';
 
+const LandingPage = lazy(() => import('./vendorDashboard/pages/LandingPage'));
+const Login = lazy(() => import('./vendorDashboard/components/forms/Login'));
+const Register = lazy(() => import('./vendorDashboard/components/forms/Register'));
+const AddArticle = lazy(() => import('./vendorDashboard/components/forms/AddArticle'));
+const ArticleTable = lazy(() => import('./vendorDashboard/components/ArticleTable'));
+const AddEvent = lazy(() => import('./vendorDashboard/components/forms/AddEvent'));
+const EventList = lazy(() => import('./vendorDashboard/components/EventList'));
+const AddFestival = lazy(() => import('./vendorDashboard/components/forms/AddFestival'));
+const FestivalList = lazy(() => import('./vendorDashboard/components/FestivalList'));
+const NotFound = lazy(() => import('./vendorDashboard/components/NotFound'));
+const EditFestival = lazy(() => import('./vendorDashboard/components/EditFestival'));
+const Welcome = lazy(() => import('./vendorDashboard/components/Welcome'));
+const ArticleHead = lazy(() => import('./vendorDashboard/components/ArticleHead'));
+const EventHeader = lazy(() => import('./vendorDashboard/components/EventHeader'));
+const FestivalHeader = lazy(() => import('./vendorDashboard/components/FestivalHeader'));
+const Donation = lazy(() => import('./vendorDashboard/components/Donation'));
+const Team = lazy(() => import('./vendorDashboard/components/Team'));
+const Updates = lazy(() => import('./vendorDashboard/components/Updates'));
 
 const App = () => {
     const navigate = useNavigate();
-    
 
     const fetchFestivals = async () => {
         try {
@@ -82,29 +81,31 @@ const App = () => {
         console.log('Edit mode set to:', mode);
     };
 
-    
     return (
-        <Routes>
-            <Route path="/" element={<LandingPage handleCreateArticle={handleCreateArticle} handleUpdateArticle={handleUpdateArticle} />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/add-article" element={<AddArticle onSubmit={handleCreateArticle} onUpdate={handleUpdateArticle} />} />
-            <Route path="/article-list" element={<ArticleTable header={<ArticleHead />} onEdit={handleUpdateArticle} onDelete={handleCreateArticle}/>} />
-            <Route path="/add-event" element={<AddEvent onSubmit={handleCreateEvent} onUpdate={handleUpdateEvent} onEditSuccess={handleEditSuccess}/>} />
-            <Route path="/event-list" element={<EventList  eventheader = {< EventHeader />} />} />
-            <Route path="/add-festival" element={<AddFestival fetchFestivals={fetchFestivals} setEditMode={setEditMode}/>} />
-            <Route path="/festival-list" element={<FestivalList festivalheader = {< FestivalHeader />} onEdit={(id) => navigate(`/edit-festival/${id}`)} />} />
-            <Route path="/edit-festival/:id" element={<EditFestival fetchFestivals={fetchFestivals} />} />
-            <Route path="*" element={<NotFound />} />
-            <Route path="/welcome" element={<Welcome />} />
-            <Route path="/donation" element={<Donation />} />
-            <Route path= "/team" element={<Team/>}/>
-            <Route path='/updates' element={<Updates/>}/>
-            
-        </Routes>
-       
+        <Suspense fallback={<div className="loading-spinner">Loading...</div>}>
+            <TransitionGroup>
+                <CSSTransition timeout={300} classNames="page">
+                    <Routes>
+                        <Route path="/" element={<LandingPage handleCreateArticle={handleCreateArticle} handleUpdateArticle={handleUpdateArticle} />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/add-article" element={<AddArticle onSubmit={handleCreateArticle} onUpdate={handleUpdateArticle} />} />
+                        <Route path="/article-list" element={<ArticleTable header={<ArticleHead />} onEdit={handleUpdateArticle} onDelete={handleCreateArticle}/>} />
+                        <Route path="/add-event" element={<AddEvent onSubmit={handleCreateEvent} onUpdate={handleUpdateEvent} onEditSuccess={handleEditSuccess}/>} />
+                        <Route path="/event-list" element={<EventList eventheader={<EventHeader />} />} />
+                        <Route path="/add-festival" element={<AddFestival fetchFestivals={fetchFestivals} setEditMode={setEditMode}/>} />
+                        <Route path="/festival-list" element={<FestivalList festivalheader={<FestivalHeader />} onEdit={(id) => navigate(`/edit-festival/${id}`)} />} />
+                        <Route path="/edit-festival/:id" element={<EditFestival fetchFestivals={fetchFestivals} />} />
+                        <Route path="*" element={<NotFound />} />
+                        <Route path="/welcome" element={<Welcome />} />
+                        <Route path="/donation" element={<Donation />} />
+                        <Route path="/team" element={<Team />} />
+                        <Route path="/updates" element={<Updates />} />
+                    </Routes>
+                </CSSTransition>
+            </TransitionGroup>
+        </Suspense>
     );
-    
 }
 
 export default App;
