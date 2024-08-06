2f52f63413deffff0f72c978d7bbf18401745ff3
shiva kumar <nanishiva2022001@gmail.com>
2024-08-06 15:44:10 +0530

    Your commit changes in article

--- src/vendorDashboard/components/Navbar.jsx
+++ src/vendorDashboard/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Navbar.css';
-import { FaSignInAlt, FaUserPlus, FaSignOutAlt, FaHome, FaMoon, FaSun } from 'react-icons/fa'; // Import icons
+import { FaSignInAlt, FaUserPlus, FaHome, FaMoon, FaSun } from 'react-icons/fa'; // Import icons
 
-const Navbar = ({ ShowLoginHandler, ShowRegister, isLoggedIn, handleLogout, isDarkMode, toggleDarkMode }) => {
+const Navbar = ({ ShowLoginHandler, ShowRegister, isDarkMode, toggleDarkMode }) => {
   return (
     <div className={`navSection ${isDarkMode ? 'dark' : 'light'}`}>
       <div className="dashboardIcon">
@@ -13,20 +13,12 @@ const Navbar = ({ ShowLoginHandler, ShowRegister, isLoggedIn, handleLogout, isDa
         <h1 className="animated-text">సనాతన ధర్మ్ సేవా సమితి</h1>
       </div>
       <div className="userAuth">
-        {isLoggedIn ? (
-          <span className="authItem" onClick={handleLogout}>
-            <FaSignOutAlt className="authIcon" /> Logout
-          </span>
-        ) : (
-          <>
-            <span className="authItem" onClick={ShowLoginHandler}>
-              <FaSignInAlt className="authIcon" /> Login
-            </span>
-            <span className="authItem" onClick={ShowRegister}>
-              <FaUserPlus className="authIcon" /> Register
-            </span>
-          </>
-        )}
+        <span className="authItem" onClick={ShowLoginHandler}>
+          <FaSignInAlt className="authIcon" /> Login
+        </span>
+        <span className="authItem" onClick={ShowRegister}>
+          <FaUserPlus className="authIcon" /> Register
+        </span>
         <span className="authItem" onClick={toggleDarkMode}>
           {isDarkMode ? <FaSun className="authIcon" /> : <FaMoon className="authIcon" />} 
         </span>
