319a988539c742e95397d94778c136a1126dd819
shiva kumar <nanishiva2022001@gmail.com>
2024-08-08 12:24:08 +0530

    Your commit changes in article

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -2,4 +2,4 @@
 //export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 
-export const API_URL ="import.meta.env.VITE_API_URL || https://backend-project-jmxk.onrender.com"
+export const API_URL =import.meta.env.VITE_API_URL ||"https://backend-project-jmxk.onrender.com"
