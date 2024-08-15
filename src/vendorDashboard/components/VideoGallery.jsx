c6e012c5541cc693a809e5dae95919d0b6e0b3a5
shiva kumar <nanishiva2022001@gmail.com>
2024-08-16 00:55:02 +0530

    Initial commit

--- src/vendorDashboard/components/VideoGallery.jsx
+++ src/vendorDashboard/components/VideoGallery.jsx
@@ -1,8 +1,22 @@
-// VideosPage.js
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Gallery.css';
 
 const VideosPage = ({ videoIds = [] }) => {
+  const videoRefs = useRef([]);
+
+  useEffect(() => {
+    if (videoRefs.current.length > 0) {
+      videoRefs.current.forEach((iframe, index) => {
+        iframe.addEventListener('ended', () => {
+          const nextVideo = videoRefs.current[index + 1];
+          if (nextVideo) {
+            nextVideo.src += "&autoplay=1";
+          }
+        });
+      });
+    }
+  }, [videoIds]);
+
   return (
     <div className="videos-page">
       <div className="videos-container">
@@ -10,7 +24,8 @@ const VideosPage = ({ videoIds = [] }) => {
           videoIds.map((id, index) => (
             <div key={index} className="video-card">
               <iframe
-                src={`https://www.youtube.com/embed/${id}`}
+                ref={(el) => (videoRefs.current[index] = el)}
+                src={`https://www.youtube.com/embed/${id}?autoplay=1&controls=0&showinfo=0&rel=0&modestbranding=1&loop=1`}
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
